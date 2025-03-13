990bb2724f0d3818ddb60b3d11d33e892e096655
Barbara Poblete <bpobletem96@gmail.com>
2025-03-13 17:47:34 -0300

    add error message and change bg to white

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   const [transcript, setTranscript] = useState('');
   const [loading, setLoading] = useState(false);
   const [copied, setCopied] = useState(false);
+  const [error, setError] = useState('');
 
   const handleCopy = async () => {
     try {
@@ -19,6 +20,7 @@ export default function Home() {
 
   async function downloadVideo(ytLink: string) {
     setLoading(true);
+    setError('');
 
     try {
       if (!ytLink) {
@@ -46,28 +48,28 @@ export default function Home() {
       setTranscript(data.text);
     } catch (error) {
       console.error('Error:', error);
-      setTranscript(`Error: ${error instanceof Error ? error.message : String(error)}`);
+      setError(`${error instanceof Error ? error.message : String(error)}`);
     } finally {
       setLoading(false);
     }
   }
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center content-center h-screen w-screen pb-10 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-radial-[at_10%_95%] from-slate-700 via-gray-800 to-slate-900 to-90% bg-fixed bg-no-repeat scroll-smooth">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center content-center h-screen w-screen pb-10 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-radial-[at_40%_75%] from-slate-100 to-sky-100 to-90% bg-no-repeat scroll-smooth">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
-        <h1 className="flex text-3xl md:text-5xl font-extrabold mb-2 md:mb-8 bg-gradient-to-bl from-white to-gray-300 bg-clip-text text-transparent">YouTube to Text</h1>
-        <p className="text-center text-gray-300 max-w-md mb-2 md:mb-8">
-          Paste a YouTube link to get its transcript. A simple way to extract and read the content of any video.
+        <h1 className="flex text-3xl md:text-5xl font-extrabold text-slate-800">YouTube to Text</h1>
+        <p className="text-center text-slate-600 max-w-md mb-2 md:mb-8">
+          Paste a YouTube link to get its transcript. Transcript any video in seconds!
         </p>
         <div className='flex items-center gap-2 md:gap-4 mb-2 md:mb-8'>
           <input
-            className='flex p-2 bg-gray-900 rounded-md border border-gray-700'
-            placeholder='Paste the youtube link'
+            className='flex p-2 bg-slate-100 rounded-md border border-slate-500 text-slate-700'
+            placeholder='YouTube link'
             value={ytLink}
             onChange={(e) => setYtLink(e.target.value)}
           ></input>
           <button
-            className='flex py-2 px-4 items-center bg-gray-900 border border-gray-700 rounded-md hover:bg-slate-700 hover:cursor-pointer transition duration-300'
+            className='flex py-2 px-4 items-center bg-slate-900 border rounded-md hover:bg-slate-700 hover:cursor-pointer transition duration-300'
             onClick={() => downloadVideo(ytLink)}
             disabled={loading}>
             {loading ? (
@@ -80,12 +82,14 @@ export default function Home() {
             )}
           </button>
         </div>
+        { error ? ( <div className='text-red-700'>{ error }</div> ) : null }
         {transcript.length > 0 && (
-          <div className="relative max-w-2xl bg-gray-800 rounded-md backdrop-blur-sm bg-opacity-10 border border-gray-500">
-            <div className="sticky top-0 w-full flex justify-end bg-gray-900 border-b border-gray-700 rounded-t-md">
+          <div className="relative max-w-2xl bg-gray-800 rounded-md backdrop-blur-sm border border-gray-500">
+            <div className="sticky top-0 w-full flex justify-between items-center bg-gray-900 border-b border-gray-700 rounded-t-md">
+              <h3 className='ms-4 text-md'>Transcript</h3>
               <button
                 onClick={handleCopy}
-                className="mx-5 py-1.5 text-sm transition-colors hover:cursor-pointer flex items-center gap-2"
+                className="me-4 py-1.5 text-sm transition-colors hover:cursor-pointer flex items-center gap-1"
               >
                 {copied ? (
                   <>
@@ -110,7 +114,7 @@ export default function Home() {
           </div>
         )}
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center text-slate-700">
         <p>by <a className='underline' href="https://bppm.dev" target="_blank">bppm</a></p>
       </footer>
     </div>
